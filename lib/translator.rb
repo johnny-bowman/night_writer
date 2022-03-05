5713cf44a9e58858a713a58c984db4863f72d442
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-04 23:10:07 -0700

    Feat: Translator prints all characters in text

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,21 @@ class Translator
     @alphabet = alphabet
   end
 
+  def full_braille_rows(text)
+    braille_lines = "#{bump_line(text, 0, 1)}#{bump_line(text, 2, 3)}#{bump_line(text, 4, 5)}".chars
+    x = braille_lines.each_slice((text.length) * 2).to_a
+  end
+
   def print_braille(text)
-    "#{bump_line(text, 0, 1)}\n#{bump_line(text, 2, 3)}\n#{bump_line(text, 4, 5)}"
+    text_chunks = full_braille_rows(text)
+    words = ""
+    until text_chunks[0].empty?
+      text_chunks.each do |chunk|
+        words << "#{chunk[0..79].join}\n"
+        chunk.slice!(0..79)
+      end
+    end
+    words
   end
 
 end
