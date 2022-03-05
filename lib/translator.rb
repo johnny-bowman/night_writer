59c0ca64a324567580eab50c602abcd0197d0b07
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-04 19:50:44 -0700

    Feat: Translator reads letters hash from braille_helper

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,12 @@
 require 'pry'
-require './braille_helper'
+require './lib/braille_helper'
 
 class Translator
   include BrailleHelper
+  attr_reader :alphabet
+
+  def initialize(alphabet = letters)
+    @alphabet = alphabet
+  end
+
 end
