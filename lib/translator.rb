0176783eb10a7c358e13f546fb80c34bd2dd4d5d
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-07 09:45:08 -0700

    Refactor full_braille_rows variable names

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,15 +10,15 @@ class Translator
   end
 
   def full_braille_rows(text)
-    braille_lines = "#{bump_line(text, 0, 1)}#{bump_line(text, 2, 3)}#{bump_line(text, 4, 5)}".chars
-    x = braille_lines.each_slice((text.length) * 2).to_a
+    full_braille_lines = "#{bump_line(text, 0, 1)}#{bump_line(text, 2, 3)}#{bump_line(text, 4, 5)}".chars
+    three_seperated_braille_rows = full_braille_lines.each_slice((text.length) * 2).to_a
   end
 
   def print_braille(text)
-    text_chunks = full_braille_rows(text)
+    braille_chunks = full_braille_rows(text)
     words = ""
-    until text_chunks[0].empty?
-      text_chunks.each do |chunk|
+    until braille_chunks[0].empty?
+      braille_chunks.each do |chunk|
         words << "#{chunk[0..79].join}\n"
         chunk.slice!(0..79)
       end
