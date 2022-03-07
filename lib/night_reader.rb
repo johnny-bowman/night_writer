eda98843d9850124364afcf8efdb828e6f886eeb
Johnny Bowman <johntbowman.production@gmail.com>
2022-03-07 09:55:45 -0700

    Feat: night_reader.rb interacts with command line

---
+++ lib/night_reader.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require './lib/translator'
+translator = Translator.new
+
+handle = File.open(ARGV[0], "r")
+braille_text = handle.read
+handle.close
+
+new_file_text = braille_text.chomp
+english = File.open(ARGV[1], "w")
+english.write(new_file_text)
